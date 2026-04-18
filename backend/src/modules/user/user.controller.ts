0587aed36faa418e806ea101c1e88dd8cb4ca144
HtetOoNaing <htetoonaing.ycc@gmail.com>
2026-04-18 11:56:35 +0700

    refactor: remove try catch from user controller as express 5 handles async error automatically

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -1,24 +1,16 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import { UserService } from "./user.service";
 
 const service = new UserService();
 
 export class UserController {
-  async create(req: Request, res: Response, next: NextFunction) {
-    try {
-      const user = await service.createUser(req.body);
-      res.status(201).json(user);
-    } catch (err) {
-      next(err);
-    }
+  async create(req: Request, res: Response) {
+    const user = await service.createUser(req.body);
+    res.status(201).json(user);
   }
 
-  async getAll(_req: Request, res: Response, next: NextFunction) {
-    try {
-      const users = await service.getUsers();
-      res.json(users);
-    } catch (err) {
-      next(err);
-    }
+  async getAll(_req: Request, res: Response) {
+    const users = await service.getUsers();
+    res.json(users);
   }
 }
